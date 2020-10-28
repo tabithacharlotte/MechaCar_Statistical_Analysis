940752fd5506a74cb54a74bca7165bc881ef636b
tabithacharlotte <tabithacharlotte24@gmail.com>
2020-10-27 18:53:05 -0700

    lot summary and total summary R file

--- MechaCarAnalysis/MechaCarChallenge.R
+++ MechaCarAnalysis/MechaCarChallenge.R
@@ -4,3 +4,7 @@ head(MechaCar_mpg)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
 
+library(tidyverse)
+Suspension_coil <- read.csv(file = 'Resources/Suspension_Coil.csv')
+total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), groups='keep') 
